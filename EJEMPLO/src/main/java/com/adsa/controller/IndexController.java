512a914fef29a5255c62b518ddad771f4f625088
dgrimaldi <dgrimaldi@LT_PORTAL01.ADSA.local>
2023-01-19 17:31:32 -0600

    Downgrade function

--- EJEMPLO/src/main/java/com/adsa/controller/IndexController.java
+++ EJEMPLO/src/main/java/com/adsa/controller/IndexController.java
@@ -1,11 +1,8 @@
 package com.adsa.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.adsa.service.FetchService;
 
 @Controller
 public class IndexController {
